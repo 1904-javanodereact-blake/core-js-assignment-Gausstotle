4d88e1ac0ed1f1bc5767533ebbc7ed6d92d91d20
unknown <bradleypelton@gmail.com>
2019-04-03 15:57:16 -0400

    problem 3 solved

--- problems/03-reverse-string/03-reverse-string.js
+++ problems/03-reverse-string/03-reverse-string.js
@@ -2,5 +2,14 @@
 Define function: reverseStr(someStr)
 Reverse and return the String. */
 function reverseStr(someStr) {
-  
+    let rtSemos = ''
+    for(let i = someStr.length -1; i > -1; i--){
+        console.log(i)
+        rtSemos += someStr[i];
+        console.log(rtSemos);
+    }
+  return rtSemos
 }
+
+let myStr = 'Hello WOrld!'
+console.log(reverseStr(myStr))
