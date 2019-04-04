1386c7bd26f1e56b0f322831a6df90e5e3dad0ce
unknown <bradleypelton@gmail.com>
2019-04-04 09:14:57 -0400

    problem 7 solved

--- problems/07-palindrome/07-palindrome.js
+++ problems/07-palindrome/07-palindrome.js
@@ -1,6 +1,27 @@
 /* 7. Palindrome
 Define function isPalindrome(someStr)
 Return true if someStr is a palindrome, otherwise return false */
-function isPalindrome(someStr) {
-  
+
+
+function reverseStr(someStr) {
+    let rtSemos = ''
+    for(let i = someStr.length -1; i > -1; i--){
+        rtSemos += someStr[i];
+    }
+  return rtSemos
 }
+
+function isPalindrome(someStr) {
+    if(someStr === reverseStr(someStr)){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+console.log(isPalindrome('bob'))
+console.log(isPalindrome('bobb'))
+console.log(isPalindrome('bob'))
+console.log(isPalindrome('b"o"b'))
+console.log(isPalindrome('bob'))
